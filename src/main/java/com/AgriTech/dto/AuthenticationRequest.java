dfd912e92fbc65479ee8282beb242d9a19483490
Nilam <nilammane498@gmail.com>
2025-01-05 16:43:40 +0530

    DTO Changes

--- src/main/java/com/AgriTech/dto/AuthenticationRequest.java
+++ src/main/java/com/AgriTech/dto/AuthenticationRequest.java
@@ -1,14 +1,15 @@
 package com.AgriTech.dto;
 
 public class AuthenticationRequest {
-	 private String user;
+	 private String username;
 	 private String password;
 	
-	public String getUser() {
-		return user;
+	
+	public String getUsername() {
+		return username;
 	}
-	public void setUser(String user) {
-		this.user = user;
+	public void setUsername(String username) {
+		this.username = username;
 	}
 	public String getPassword() {
 		return password;
